73323e6aa49ba7bb4f09f3414fea218fe20d6cf9
Mahmoud Zuhgbor <43893822+mzughbor@users.noreply.github.com>
2022-06-23 02:44:24 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ function builtTweet() {
         newFeedTweet.className = 'feed-tweet';
         /*newFeedTweet.setAttribute('flex-direction', 'column');*/
 
-        let newDiv = document.createElement('div'); ///*
+        let newDiv = document.createElement('div'); ///* root
         newFeedTweet.append(newDiv);
         newDiv.className = 'tweet-img'
 
@@ -25,11 +25,11 @@ function builtTweet() {
         userimg.className = 'tweet-img';
         userimg.setAttribute('src', './myprityface.jpg')
 
-        let newFeedTweetdetails= document.createElement('div');///*
+        let newFeedTweetdetails= document.createElement('div');///* root
         newFeedTweet.append(newFeedTweetdetails);
         newFeedTweetdetails.className = 'feed-tweet-details';
 
-        let TweeterDetailsdiv = document.createElement('div');//
+        let TweeterDetailsdiv = document.createElement('div');// same
         //newFeedTweetdetails.prepend(TweeterDetailsdiv);
         TweeterDetailsdiv.className = 'tweeter-details';
         newFeedTweetdetails.append(TweeterDetailsdiv);
@@ -44,7 +44,7 @@ function builtTweet() {
         spanName.className = 'tweeter-handle';
         spanName.textContent = '@mzughbor ~ 1.min';
 
-        let TweetTextClass = document.createElement('div');//
+        let TweetTextClass = document.createElement('div');// same
         //newFeedTweetdetails.prepend(TweetTextClass);
         TweetTextClass.className = 'tweet-text';
         newFeedTweetdetails.appendChild(TweetTextClass);
@@ -53,52 +53,40 @@ function builtTweet() {
         TweetTextClass.appendChild(newText)
         newText.textContent = tweet.tweetText;
 
-        let listIcons = document.createElement('div');//
+        let listIcons = document.createElement('div');// same
         //newFeedTweetdetails.prepend(listIcons);
-        listIcons.className = "tweet-icons"
+        listIcons.className = "tweet-icons";
         newFeedTweetdetails.appendChild(listIcons);
 
-        let icons = document.createElement('svg');
-        icons.className = "main-svg"
+        //let newElement = document.createElementNS("http://www.w3.org/2000/svg", 'path'); //Create a path in SVG's namespace
+        let icons = document.createElement('svg');      
+        icons.className = "main-svg";
         icons.setAttribute('viewBox', '0 0 24 24');
         listIcons.appendChild(icons);
 
         let gtag = document.createElement('g');
+        gtag.className = 'myg';
+
         icons.appendChild(gtag);
-
-        let pathTag = document.createElement('path');
-        pathTag.setAttribute('d', 'M14.046 2.242l-4.148-.01h-.002c-4.374 0-7.8 3.427-7.8 7.802 0 4.098 3.186 7.206 7.465 7.37v3.828c0 .108.044.286.12.403.142.225.384.347.632.347.138 0 .277-.038.402-.118.264-.168 6.473-4.14 8.088-5.506 1.902-1.61 3.04-3.97 3.043-6.312v-.017c-.006-4.367-3.43-7.787-7.8-7.788zm3.787 12.972c-1.134.96-4.862 3.405-6.772 4.643V16.67c0-.414-.335-.75-.75-.75h-.396c-3.66 0-6.318-2.476-6.318-5.886 0-3.534 2.768-6.302 6.3-6.302l4.147.01h.002c3.532 0 6.3 2.766 6.302 6.296-.003 1.91-.942 3.844-2.514 5.176z');
+        /*
+        let pathTag = document.createElementNS("http://www.w3.org/2000/svg", 'path');
+        pathTag.setAttribute("d", "M14.046 2.242l-4.148-.01h-.002c-4.374 0-7.8 3.427-7.8 7.802 0 4.098 3.186 7.206 7.465 7.37v3.828c0 .108.044.286.12.403.142.225.384.347.632.347.138 0 .277-.038.402-.118.264-.168 6.473-4.14 8.088-5.506 1.902-1.61 3.04-3.97 3.043-6.312v-.017c-.006-4.367-3.43-7.787-7.8-7.788zm3.787 12.972c-1.134.96-4.862 3.405-6.772 4.643V16.67c0-.414-.335-.75-.75-.75h-.396c-3.66 0-6.318-2.476-6.318-5.886 0-3.534 2.768-6.302 6.3-6.302l4.147.01h.002c3.532 0 6.3 2.766 6.302 6.296-.003 1.91-.942 3.844-2.514 5.176z");
+        //pathTag.style.stroke = "#cccbcb";   
+        //pathTag.style.strokeWidth = "1px";  
         gtag.appendChild(pathTag);
-
-        let likeIcon = document.createElement('svg');
-        likeIcon.addEventListener('click', (e) => {
-            if (e.currentTarget.parentNode.parentNode.parentNode.style.backgroundColor) {
-                return e.currentTarget.parentNode.parentNode.parentNode.style.backgroundColor = null;
-            } else {
-                return e.currentTarget.parentNode.parentNode.parentNode.style.backgroundColor = "red"
-            }
-        })
-        likeIcon.setAttribute('viewBox','0 0 24 24');
-        likeIcon.className = 'main-svg';
-        icons.appendChild(likeIcon)
-
-
-        let retweetIcon = document.createElement('svg')
-        retweetIcon.addEventListener('click', (e) => {
-            let author = e.currentTarget.parentNode.parentNode.parentNode.childNodes[0].textContent;
-            let tweetText = e.currentTarget.parentNode.parentNode.parentNode.childNodes[1].textContent;
-            let tweetDetails = { author: author, tweetText };
-            arrayOfTweets.push(tweetDetails);
-            tweetsBox.textContent = "";
-            builtTweet();
-        })
-        retweetIcon.setAttribute('viewBox','0 0 24 24');
-        retweetIcon.className = 'main-svg'
-        icons.appendChild(retweetIcon)
-
+        */
+        
+        const svg = document.querySelector('.myg');  
+        console.log(svg);
+        const newElement = document.createElementNS("http://www.w3.org/2000/svg", 'path');  
+        newElement.setAttribute("d", "M14.046 2.242l-4.148-.01h-.002c-4.374 0-7.8 3.427-7.8 7.802 0 4.098 3.186 7.206 7.465 7.37v3.828c0 .108.044.286.12.403.142.225.384.347.632.347.138 0 .277-.038.402-.118.264-.168 6.473-4.14 8.088-5.506 1.902-1.61 3.04-3.97 3.043-6.312v-.017c-.006-4.367-3.43-7.787-7.8-7.788zm3.787 12.972c-1.134.96-4.862 3.405-6.772 4.643V16.67c0-.414-.335-.75-.75-.75h-.396c-3.66 0-6.318-2.476-6.318-5.886 0-3.534 2.768-6.302 6.3-6.302l4.147.01h.002c3.532 0 6.3 2.766 6.302 6.296-.003 1.91-.942 3.844-2.514 5.176z");   
+        newElement.setAttribute("class", "sss")
+        svg.appendChild(newElement);
+        // i'm really confused about this issue I have set up and looked about it in consloe every thing is great but still not shown from the first time -- the thing confusing me her if i edit the svg as an HTML element this svg will shown right away ! so whats the issue ????? oh !
     })
 }
 
+// this is done //
 tweetButton.addEventListener('click', (e) => {
     let tweetDetails = {
         author: author,
@@ -109,7 +97,5 @@ tweetButton.addEventListener('click', (e) => {
     tweetsBox.textContent = "";
     builtTweet()
     newTweetText.value = ""
-
-
 });
 
